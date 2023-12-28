98c95d7e9ebb965c6390216d30cf7ebeda5f4817
luminecs <wangxuke.china@gmail.com>
2023-12-28 21:06:24 +0800

    commit

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,10 +1,12 @@
-import { Link } from '@remix-run/react'
+import { Link } from '@remix-run/react';
 
-export default function Header () {
+export default function Header() {
   return (
     <div className="header">
-      <Link to="/" className="home-link">Home</Link>
-      <button className="reader">Read</button>
+      <Link to="/" className="home-link">
+        Home
+      </Link>
+      {/* <button className="reader">Read</button> */}
     </div>
-  )
-};
+  );
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,23 +1,21 @@
-import type { MetaFunction } from '@remix-run/cloudflare'
-import Questions from '~/routes/questions/questions'
-import CodeBlock from '~/components/code-block'
-import { base16AteliersulphurpoolLight as theme } from 'react-syntax-highlighter/dist/esm/styles/prism'
-import Header from '~/components/header'
+import type { MetaFunction } from '@remix-run/cloudflare';
+import Questions from '~/routes/questions/questions';
+import CodeBlock from '~/components/code-block';
+import { base16AteliersulphurpoolLight as theme } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import Header from '~/components/header';
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'luminecs notes' }, { name: 'description', content: 'Welcome to luminecs notes!' }]
-}
+  return [{ title: 'luminecs notes' }, { name: 'description', content: 'Welcome to luminecs notes!' }];
+};
 
-export default function Index () {
+export default function Index() {
   return (
     <>
-      <Header/>
-      <div style={{ marginTop: '4rem'}}>
-        <CodeBlock code={'No questions, no answers.'} theme={theme} hideCopyButton={true}/>
+      <Header />
+      <div style={{ marginTop: '4rem' }}>
+        <CodeBlock code="No questions, no answers." theme={theme} hideCopyButton={true} />
       </div>
-      <>
-        <Questions/>
-      </>
+      <Questions />
     </>
-  )
+  );
 }
