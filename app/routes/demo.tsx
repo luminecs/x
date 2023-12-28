8434de61b9eaad212325a4746f5b43e179720460
luminecs <wangxuke.china@gmail.com>
2023-12-28 14:24:30 +0800

    commit

--- app/routes/demo.tsx
+++ app/routes/demo.tsx
@@ -2,8 +2,8 @@ import CodeBlock from '~/components/code-block'
 import type { MetaFunction } from '@remix-run/cloudflare'
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'My Remix Demo' }, { name: 'description', content: 'Welcome to My Remix Demo!' }];
-};
+  return [{ title: 'My Remix Demo' }, { name: 'description', content: 'Welcome to My Remix Demo!' }]
+}
 
 const c = `export default function Demo () {
   return (
@@ -24,11 +24,7 @@ const c = `export default function Demo () {
 export default function Demo () {
   return (
     <div>
-      <h2>
-        <a id="Welcome" href="#1">
-          Welcome to Remix
-        </a>
-      </h2>
+      <h2><a>Welcome to Remix</a></h2>
       <CodeBlock code={'let a = 1;'} lang={'javascript'}/>
       <CodeBlock code={`var a = 1;\nconst b = \'hallo\'`} lang={'javascript'}/>
       <CodeBlock code={c} lang={'javascript'}/>
